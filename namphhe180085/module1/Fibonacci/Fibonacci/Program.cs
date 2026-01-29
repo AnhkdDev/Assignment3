3fe13c259888f577cf710928e34b36e1546ac37e
devnam1401 <namphhe180085@fpt.edu.vn>
2026-01-29 11:20:11 +0700

    Implement Fibonacci using loop when flag = false

--- namphhe180085/module1/Fibonacci/Fibonacci/Program.cs
+++ namphhe180085/module1/Fibonacci/Fibonacci/Program.cs
@@ -4,7 +4,32 @@
     {
         public interface IFibonacci
         {
-            int Fun(int n, bool flag);
+            public long Fibonacci(int n, bool flag)
+            {
+                if (flag)
+                {
+                    return -1;
+                }
+
+                if (n <= 1)
+                {
+                    return n;
+                }
+
+                long a = 0;
+                long b = 1;
+                long result = 0;
+
+                for (int i = 2; i <= n; i++)
+                {
+                    result = a + b;
+                    a = b;
+                    b = result;
+                }
+
+                return result;
+            }
+
         }
     }
 }
